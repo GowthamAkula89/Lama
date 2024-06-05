fc77daca672e14fd460756bc0136edffd7f3f947
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-06-05 23:19:56 +0530

    Add services

---
+++ backend/src/services/projects.service.js
@@ -0,0 +1,32 @@
+const httpStatus = require("http-status");
+const ApiError = require("../utils/ApiError");
+const mongoose = require("mongoose");
+const {ProjectsData} = require("../models/offering.model")
+const addProject = async(projectDetails)=>{
+    try {
+        const project = await ProjectsData.create(projectDetails);
+        return project;
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            throw new ApiError(httpStatus.BAD_REQUEST, "Validation error: " + error.message);
+        } else if (error.code === 11000) {
+
+            
+            throw new ApiError(httpStatus.BAD_REQUEST, "Website name already exists");
+        } else {
+            throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred");
+        }
+    }
+}
+const getProject = async() => {
+    const project = await ProjectsData.find({}).catch((error) => {
+        if (mongoose.Error.ValidationError) {
+          throw new ApiError(httpStatus.BAD_REQUEST, "Filed to fetch the data");
+        }
+    });
+    return project;
+}
+module.exports = {
+    addProject,
+    getProject
+}
