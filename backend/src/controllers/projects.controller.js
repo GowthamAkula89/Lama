4c8bfac6415f82a14e6157b1d7351b512e391ae1
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-06-05 23:15:00 +0530

    Add controller

---
+++ backend/src/controllers/projects.controller.js
@@ -0,0 +1,15 @@
+const catchAsync=require("../utils/catchAsync");
+const projectsServices = require("../services/offering.service")
+const getProject = catchAsync(async (req, res) => {
+    const offerings = await projectsServices.getOfferings();
+    res.json({offerings});
+});
+const addProject = catchAsync( async(req,res)=>{
+    const offering = await projectsServices.addOffering(req.body);
+    res.send(offering);
+
+})
+module.exports = {
+    getProject,
+    addProject
+}
