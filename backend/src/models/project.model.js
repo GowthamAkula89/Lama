2cee16aab3a5dce681d78f41adf3b9e0e105d6d7
Akula Gowtham <gowtham.akula@udtrucks.com>
2024-06-06 11:03:34 +0530

    Update project model

--- backend/src/models/project.model.js
+++ backend/src/models/project.model.js
@@ -5,6 +5,46 @@ const projectSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true
+    },
+    files:[{
+        fileName:{
+            type: String,
+            required: true,
+            trim: true
+        },
+        fileDiscription: {
+            type: String,
+            required: true,
+            trim: true
+        }
+    }],
+    configuration:{
+        general:{
+            chatBotName:{
+                type: String,
+                required: true,
+                trim: true
+            },
+            welcomeMessage:{
+                type: String,
+                required: true,
+                trim: true
+            },
+            inputPlaceholder:{
+                type: String,
+                required: true,
+                trim: true
+            }
+        },
+        display:{
+            primaryColor: String,
+            fontColor: String,
+            fontSize : Number,
+            chatIconSize : String,
+            positionScreen: String,
+            distBottom: Number,
+            horizontalDist: Number,
+        }
     }
 });
 const ProjectssData = mongoose.model("ProjectssData", projectSchema);
