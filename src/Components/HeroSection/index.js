8a6e123a4d82715dddae18ea7c793aa4db40df07
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-06-09 09:10:09 +0530

    Fix Responsiveness

--- src/Components/HeroSection/index.js
+++ src/Components/HeroSection/index.js
@@ -8,89 +8,103 @@ import ProjectModal from "../ProjectModal";
 import DataContext from "../DataContext";
 
 export function projectCard(project) {
-    return (
-        <div className="project-card">
-            <div className="project-title">{project.projectName.slice(0, 2).toUpperCase()}</div>
-            <div className="project-details">
-                <div>
-                    <div className="project-name">{project.projectName}</div>
-                    <div>{`${project.files.length} Files`}</div>
-                </div>
-                <div className="project-update">Last edited just now</div>
-            </div>
+  return (
+    <div className="project-card">
+      <div className="project-title">
+        {project.projectName.slice(0, 2).toUpperCase()}
+      </div>
+      <div className="project-details">
+        <div>
+          <div className="project-name">{project.projectName}</div>
+          <div>{`${project.files.length} Files`}</div>
         </div>
-    );
+        <div className="project-update">Last edited just now</div>
+      </div>
+    </div>
+  );
 }
 
 const HeroSection = () => {
-    const { projects, setProjects, setProject } = useContext(DataContext);
-    const [showModal, setShowModal] = useState(false);
-    const [loading, setLoading] = useState(true); // Add loading state
+  const { projects, setProjects, setProject } = useContext(DataContext);
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(true); // Add loading state
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(config.endpoint);
-                const data = await response.json();
-                setProjects(data.projects);
-                setLoading(false); // Set loading to false after data is fetched
-            } catch (error) {
-                console.error('Error fetching projects:', error);
-                setLoading(false); // Set loading to false in case of error
-            }
-        };
-        fetchData();
-    }, [setProjects]); // Add setProjects as a dependency
-
-    const handleShowModal = () => {
-        setShowModal(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(config.endpoint);
+        const data = await response.json();
+        setProjects(data.projects);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+        setLoading(false);
+      }
     };
+    fetchData();
+  }, [setProjects]);
 
-    const handleProject = (data) => {
-        setProject(data);
-    };
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
 
-    if (loading) {
-        return <div className="loading-message">Loading...</div>; 
-    }
+  const handleProject = (data) => {
+    setProject(data);
+  };
 
-    return (
-        <>
-            {projects.length === 0 ? (
-                <div className="hero-container">
-                    <div className="hero-heading">Create a New Project</div>
-                    <img className="hero-img" src={HeroImg} alt="img" />
-                    <div className="hero-content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                    </div>
-                    <div className="create-btn" onClick={handleShowModal}>
-                        <img src={AddIcon} alt="add-icon" className="add-icon" />
-                        <div className="create-btn-text">Create New Project</div>
-                    </div>
-                </div>
-            ) : (
-                <div className="projects-container">
-                    <div className="projects-header">
-                        <div className="project-heading">Projects</div>
-                        <div className="create-btn" onClick={handleShowModal}>
-                            <img src={AddIcon} alt="add-icon" className="add-icon2" />
-                            <div className="create-btn-text2">Create New Project</div>
-                        </div>
-                    </div>
-                    <div className="projects-list">
-                        {projects.map((project) =>
-                            <div key={project._id}>
-                                <Link to="/project" className="project-container" onClick={() => handleProject(project)}>
-                                    {projectCard(project)}
-                                </Link>
-                            </div>
-                        )}
-                    </div>
-                </div>
-            )}
-            <ProjectModal showModal={showModal} setShowModal={setShowModal} setProjects={setProjects} />
-        </>
-    );
+  if (loading) {
+    return <div className="loading-message">Loading please wait...</div>;
+  }
+
+  return (
+    <>
+      {projects.length === 0 ? (
+        <div className="hero-container">
+          <div className="hero-heading">Create a New Project</div>
+          <img className="hero-img" src={HeroImg} alt="img" />
+          <div className="hero-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in
+          </div>
+          <div className="create-btn" onClick={handleShowModal}>
+            <img src={AddIcon} alt="add-icon" className="add-icon" />
+            <div className="create-btn-text">Create New Project</div>
+          </div>
+        </div>
+      ) : (
+        <div className="projects-container">
+          <div className="projects-header">
+            <div className="project-heading">Projects</div>
+            <div className="create-btn" onClick={handleShowModal}>
+              <img src={AddIcon} alt="add-icon" className="add-icon2" />
+              <div className="create-btn-text2">Create New Project</div>
+            </div>
+          </div>
+          <div className="projects-list">
+            {projects.map((project) => (
+              <div key={project._id}>
+                <Link
+                  to="/project"
+                  className="project-container"
+                  onClick={() => handleProject(project)}
+                >
+                  {projectCard(project)}
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      <ProjectModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        setProjects={setProjects}
+      />
+    </>
+  );
 };
 
 export default HeroSection;
--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sideBar.css";
 import { Link } from "react-router-dom";
 import SettingIcon from "../Utils/settingIcon.png";
-const SideBar = ({isProjectsPage, isConfigurationPage, isSettingsPage}) => {
-    return(
-        <div className="side-bar">
-            <div className="menu-list">
-                <div className="menu-list-heading">Podcast Upload Flow</div>
-                <Link to="/project" className="project-container">
-                    <div className={`menu-item ${isProjectsPage?"projects":""}`}>
-                        <div className="slno">1</div>
-                        <div className="menu-item-name">Projects</div>
-                    </div>
-                </Link>
-                <Link to="/configurations" className="project-container">
-                    <div className={`menu-item ${isConfigurationPage?"configurations":""}`}>
-                        <div  className="slno">2</div>
-                        <div  className="menu-item-name">Widget Configurations</div>
-                    </div>
-                </Link>
+
+const SideBar = ({ isProjectsPage, isConfigurationPage, isSettingsPage }) => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  return (
+    <div className="side-bar-container">
+      <div className="mobile-view-menu" onClick={toggleSidebar}>
+        Menu
+      </div>
+      <div className={`side-bar ${showSidebar ? "show" : ""}`}>
+        <div className="menu-list">
+          <div className="menu-list-heading">Podcast Upload Flow</div>
+          <Link to="/project" className="project-container">
+            <div className={`menu-item ${isProjectsPage ? "projects" : ""}`}>
+              <div className="slno">1</div>
+              <div className="menu-item-name">Projects</div>
             </div>
-            <Link to="/settings" className="project-container">
-                <div className={`menu-item ${isSettingsPage?"settings":""}`}>
-                    <img src={SettingIcon} alt="setting-img"/>
-                    <div className="menu-item-name">Settings</div>
-                </div>
-            </Link>
+          </Link>
+          <Link to="/configurations" className="project-container">
+            <div
+              className={`menu-item ${
+                isConfigurationPage ? "configurations" : ""
+              }`}
+            >
+              <div className="slno">2</div>
+              <div className="menu-item-name">Widget Configurations</div>
+            </div>
+          </Link>
         </div>
-    )
-}
+        <Link to="/settings" className="project-container">
+          <div className={`menu-item ${isSettingsPage ? "settings" : ""}`}>
+            <img src={SettingIcon} alt="setting-img" />
+            <div className="menu-item-name">Settings</div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 export default SideBar;
